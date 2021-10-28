2e0a085f2a38d6cc9fedecba32200745093e0f35
Jeff Kimball <jeffkimballis@gmail.com>
2021-10-28 10:18:47 -0500

    refactor App component

--- src/index.js
+++ src/index.js
@@ -3,25 +3,15 @@ import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-    constructor(props){
-        super(props)
-        
-        this.state = { 
-            lat: null,
-            errorMessage:''
-        }
-
+            
+    state = { lat: null, errorMessage:''  }     
+    
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({lat: position.coords.latitude})
-            },
-            err => {
-                this.setState({errorMessage: err.message})
-            }
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})            
         )
     }
-    
-    
 
     render(){
 
